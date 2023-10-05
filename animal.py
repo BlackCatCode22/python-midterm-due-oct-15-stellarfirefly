0de66dd2a7fca281e3c328f73f578527a93f8885
Jay C. Parangalan <stellarfirefly@yahoo.com>
2023-10-04 20:29:31 -0700

    added name and species
    - Added functionality to handle animal names and species.
    - Updated test harness to show species.

--- animal.py
+++ animal.py
@@ -5,7 +5,10 @@ import calendar             # used for various date functions
 #----
 # Animal class that contains all things common to all animals.
 # In this case, a birthday, including a way to generate one and to
-# retrieve it in human-readable format.
+# retrieve it in human-readable format. Also a species and a name.
+#
+# The species is intended to simply be a generic descriptor for the
+# animal, e.g. lion, tiger, bear, etc.
 #
 class Animal:
             # dictionary for valid months of a season,
@@ -16,8 +19,35 @@ class Animal:
         "fall":     [9, 10, 11],
         "winter":   [12, 1, 2],
     }
-    def __init__(self):
+
+    #----
+    # Constructor, must specify at least a species. Can also specify
+    # a name if it is known at construction time.
+    #
+    def __init__(self, species, name = "unnamed"):
         self.current_year = date.today().year   # find and stash the current year
+        self.species = species
+        self.name = name
+
+    #----
+    # Set a name, if it wasn't known at creation time, or if it was
+    # changed at some point.
+    #
+    def set_name(self, name):
+        self.name = name
+
+    #----
+    # Get the name. Yeah, Python has no variable hiding, but let's try to
+    # be good little programmers and make a getter.
+    #
+    def get_name(self):
+        return self.name
+
+    #----
+    # Get the animal's species, set at construction.
+    #
+    def get_species(self):
+        return self.species
 
     #----
     # Generate a random birthdate for an animal, based upon a given age
@@ -53,9 +83,11 @@ class Animal:
 
 if __name__ == "__main__":      # for testing the class
     random.seed()
-    creature = Animal()
-    print("current year:", creature.current_year)
-    creature.genBirthDay(2, "unknown")
-    print("random birthday of 2-yr-old, unknown season:", creature.getBirthday())
-    creature.genBirthDay(5, "winter")
-    print("random birthday of 5-yr-old, winter season:", creature.getBirthday())
+    creature1 = Animal("bear", "Yogi")  # named bear
+    creature2 = Animal("lion")          # unnamed lion
+    print("current year:", creature1.current_year)
+    creature1.genBirthDay(2, "unknown")
+    print("random birthday of 2-yr-old", creature1.get_species(), ", unknown season:", creature1.getBirthday())
+    creature2.genBirthDay(5, "winter")
+    print("random birthday of 5-yr-old", creature2.get_species(), ", winter season:", creature2.getBirthday())
+
